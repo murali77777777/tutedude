facb948118cdd4aa4d95f34e446f7830f9cc138a
murali77777777 <muralimacharla70@gmail.com>
2025-08-08 00:28:37 +0530

    Add files via upload

---
+++ assissgnment.py
@@ -0,0 +1,7 @@
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multipication:",a*b)
+print("Division:",a/b)
---
+++ assissgnment1.py
@@ -0,0 +1,6 @@
+# create a personalized greeting
+
+a=input("enter your first name")
+b=input("enter your last name")
+print(a+b)
+print("hello,",a,b ,"! welcome to python program.")
